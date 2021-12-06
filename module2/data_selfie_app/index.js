3fd84eac37324679acff24d6e70e4a2c2ce5dc23
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-06 15:45:13 -0800

    update response a json object

---
+++ module2/data_selfie_app/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, () => console.log('listening at 3000'));
+
+app.use(express.static('public'));
+app.use(express.json({ limit : '1mb'}));
+
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    const data = request.body;
+    console.log(data);
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitute: data.lon
+    });
+})
