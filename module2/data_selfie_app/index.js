1eaeb3f14049ff8affa082b8969d9dff28c22ace
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-07 09:58:06 -0800

    detect error with submit button

--- module2/data_selfie_app/index.js
+++ module2/data_selfie_app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Datastore = require('nedb');
 
 const app = express();
 
@@ -7,12 +8,22 @@ app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit : '1mb'}));
 
+const  database = new Datastore('database.db');
+database.loadDatabase();
+
+
+
 app.post('/api', (request, response) => {
     console.log('I got a request!');
     const data = request.body;
-    console.log(data);
+
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    // console.log(database);
     response.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: data.lat,
         longitute: data.lon
     });
