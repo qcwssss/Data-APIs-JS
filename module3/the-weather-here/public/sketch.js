75565d18713be8692a02c787a36844918c4509ad
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-10 20:03:19 -0800

    update module3 start file

---
+++ module3/the-weather-here/public/sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+
+    let lat, lon;
+    const button = document.getElementById('submit');
+    button.addEventListener('click', async event => {
+        const mood = document.getElementById('mood').value;        
+        // load image to base64
+        video.loadPixels();
+        const data = { lat, lon, mood};
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+        const response = await fetch('api/', options);
+        const json = await response.json();
+        console.log(json);
+
+    });
+
+
+    if ('geolocation' in navigator) {
+        console.log('geolocation is available');
+        navigator.geolocation.getCurrentPosition(async position => {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            document.getElementById('latitude').textContent = lat;
+            document.getElementById('longitude').textContent = lon;
+        });
+    } else {
+        console.log('geolocation IS NOT available');
+    }
+
+}
