8a9b8ebeb0068d53313b0cad78be1411bd194885
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-11 09:53:26 -0800

    start code complete

--- module3/the-weather-here/index.js
+++ module3/the-weather-here/index.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const Datastore = require('nedb');
+// const fetch = require('node-fetch');
 
 const app = express();
-
-app.listen(3000, () => console.log('listening at 3000'));
-
+app.listen(3000, () => console.log('Starting server: http://localhost:3000'));
 app.use(express.static('public'));
-app.use(express.json({ limit : '1mb'}));
+app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
 app.get('/api', (request, response) => {
-    database.find({}, (err, data) => {
-        if(err) {
-            console.log(err);
-            response.end();
-            return;
-        }
-        response.json(data);
-
-    });
-})
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
 
 app.post('/api', (request, response) => {
-    const data = request.body;
-    const timestamp = Date.now();
-    data.timestamp = timestamp;
-    database.insert(data);
-    response.json(data);
+  const data = request.body;
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data);
+  response.json(data);
 });
+
+// app.get('/weather', async (request, response) => {
+//   const api_url = `https://api.darksky.net/forecast/08fe01a78943266193fc7a23625f68fa/37.8267,-122.4233`;
+//   const fetch_response = await fetch(api_url);
+//   const json = await fetch_response.json();
+//   response.json(json);
+// });
--- module3/the-weather-here/public/sketch.js
+++ module3/the-weather-here/public/sketch.js
@@ -1,36 +1,28 @@
-// Geo locate
 let lat, lon;
 if ('geolocation' in navigator) {
-    console.log('geolocation is available');
-    navigator.geolocation.getCurrentPosition(async position => {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        document.getElementById('latitude').textContent = lat;
-        document.getElementById('longitude').textContent = lon;
-    });
+  console.log('geolocation available');
+  navigator.geolocation.getCurrentPosition(position => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    console.log(lat, lon);
+    document.getElementById('latitude').textContent = lat.toFixed(2);
+    document.getElementById('longitude').textContent = lon.toFixed(2);
+  });
 } else {
-    console.log('geolocation IS NOT available');
+  console.log('geolocation not available');
 }
 
-const button = document.getElementById('submit');
+const button = document.getElementById('checkin');
 button.addEventListener('click', async event => {
-    const mood = document.getElementById('mood').value;
-    const data = {
-        lat,
-        lon,
-        mood
-    };
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }
-    const response = await fetch('api/', options);
-    const json = await response.json();
-    console.log(json);
-
+  const data = { lat, lon };
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+  const response = await fetch('/api', options);
+  const json = await response.json();
+  console.log(json);
 });
-
-
