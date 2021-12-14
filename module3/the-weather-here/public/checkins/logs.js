b3e4c1c6d901c3105f5830868fa16a5c6e4b3499
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-13 21:06:37 -0800

    data is empty?

--- module3/the-weather-here/public/checkins/logs.js
+++ module3/the-weather-here/public/checkins/logs.js
@@ -1,20 +1,35 @@
+// Making a map and tiles
+const myMap = L.map('checkinMap').setView([0, 0], 1);
+
+const attribution = ' &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, {
+  attribution
+});
+tiles.addTo(myMap);
 
 getData();
 
 async function getData() {
+  console.log('Hello?');
   const response = await fetch('/api');
   const data = await response.json();
+  console.log(data);
 
   for (item of data) {
-    const root = document.createElement('p');
-    const geo = document.createElement('div');
-    const date = document.createElement('div');
 
-    geo.textContent = `${item.lat}°, ${item.lon}°`;
-    const dateString = new Date(item.timestamp).toLocaleString();
-    date.textContent = dateString;
-    root.append(geo, date);
-    document.body.append(root);
+    const marker = L.marker([item.lat, item.lon]).addTo(myMap);
+    let txt = `Ther weather here is at ${item.lat}&deg;, 
+    ${item.lon}&deg; is ${item.weather.weather[0].main} with a 
+    temperature of ${item.weather.temperature}&deg; F.`
+
+    if (item.air.value < 0) {
+      txt += 'No air quality reading. ';
+    } else {
+      txt += ` The concentration of particulate matter(${item.air.parameter})
+       is ${item.air.value} ${item.air.unit} last read on ${item.air.lastUpdated}`;
+    }
+    marker.bindPopup(txt);
   }
   console.log(data);
 }
